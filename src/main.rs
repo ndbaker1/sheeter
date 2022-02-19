2d058c192c740bf1151f297feb3c5115db3bf4ac
Nick Baker <ndbaker1@outlook.com>
2022-02-19 03:31:08 -0600

    - generic conversion to Vec<f64> - removing mirror portion of rfft - adding new argument for overlap

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,14 @@ fn main() -> Result<(), Error> {
         .nth(1)
         .expect("first argument should be path to a .wav file");
 
-    let duration: f64 = env::args()
+    let chunk_size_in_seconds: f64 = env::args()
         .nth(2)
+        .expect("second argument should be a duration")
+        .parse()
+        .expect("expected a number");
+
+    let chunk_overlap: f64 = env::args()
+        .nth(3)
         .expect("third argument should be a duration")
         .parse()
         .expect("expected a number");
@@ -18,7 +24,7 @@ fn main() -> Result<(), Error> {
 
     let (header, data) = wav::read(&mut wav_file)?;
 
-    let chunk_size = (duration * header.sampling_rate as f64) as usize;
+    let chunk_size = (chunk_size_in_seconds * header.sampling_rate as f64) as usize;
     let chunk_count = header.sampling_rate as usize / chunk_size;
     let channel_count = header.channel_count as usize;
 
@@ -28,52 +34,76 @@ fn main() -> Result<(), Error> {
     let fft = FftPlanner::new().plan_fft_forward(chunk_size);
 
     let width = chunk_count;
-    let height = chunk_size;
+    let height = chunk_size / 2;
 
     let mut raster = Raster::with_clear(width as u32, height as u32);
 
-    if header.bits_per_sample == 8 {
-        // reusable iterator that will be for consecutive reads
-        let data_reader = data.try_into_eight().unwrap();
-        eprintln!("wav_data_length: {}", data_reader.len());
+    // reusable iterator that will be for consecutive reads
+    let data_reader: Vec<f64> = match header.bits_per_sample {
+        8 => data
+            .try_into_eight()
+            .unwrap()
+            .into_iter()
+            .map(|num| num as f64)
+            .collect(),
+        16 => data
+            .try_into_sixteen()
+            .unwrap()
+            .into_iter()
+            .map(|num| num as f64)
+            .collect(),
+        24 => data
+            .try_into_twenty_four()
+            .unwrap()
+            .into_iter()
+            .map(|num| num as f64)
+            .collect(),
+        32 => data
+            .try_into_thirty_two_float()
+            .unwrap()
+            .into_iter()
+            .map(|num| num as f64)
+            .collect(),
+        _ => return Ok(eprintln!("was not matching bit size")),
+    };
 
-        for chunk in 0..chunk_size {
-            // reading the data for a single channel at a time and performing FFT on it
-            let mut channel_buffers: Vec<Vec<_>> = (0..channel_count)
-                .map(|channel| {
-                    data_reader[channel..]
-                        .iter()
-                        .step_by(channel_count)
-                        .take(chunk_size)
-                        // Map smaller range integers to i64 to avoid overflows
-                        .map(|num| *num as f64)
-                        // turn the numbers into Complex form for fft library
-                        .map(Complex::from)
-                        .collect::<Vec<Complex<f64>>>()
-                })
-                .collect();
+    eprintln!("wav_data_length: {}", data_reader.len());
 
-            // use the first channel for now
-            let buffer = &mut channel_buffers[0];
-            fft.process(buffer);
-            // Find the max in the pool in order to normalize the f64 values
-            // ignore the DC component by skipping the 0th index (corresponding to no period)
-            let max_val = buffer[1..].iter().map(|c| c.re.abs()).fold(1f64, f64::max);
+    for chunk in 0..chunk_count {
+        // reading the data for a single channel at a time and performing FFT on it
+        let mut channel_buffers: Vec<Vec<_>> = (0..channel_count)
+            .map(|channel| {
+                data_reader[channel + chunk * chunk_size..]
+                    .iter()
+                    .step_by(channel_count)
+                    .take(chunk_size)
+                    // turn the numbers into Complex form for fft library
+                    .map(Complex::from)
+                    .collect()
+            })
+            .collect();
 
-            for (y, value) in buffer[1..].iter().enumerate() {
-                // normalize the Complex FFT value and use it for the color
-                let color_value = (u8::MAX as f64 * value.re.abs() / max_val) as u8;
-                // update the pixel value
-                // Coordinate {
-                //      x <-- current chunk
-                //      y <-- current row
-                // }
-                *raster.pixel_mut(chunk as i32, y as i32) =
-                    SHwb8::new(color_value / 2, color_value / 2, color_value);
-            }
+        // use the first channel for now
+        let buffer = &mut channel_buffers[0];
+        fft.process(buffer);
+        // Find the max in the pool in order to normalize the f64 values
+        // ignore the DC component by skipping the 0th index (corresponding to no period)
+        let max_val = buffer[1..height]
+            .iter()
+            .map(|c| c.re.abs())
+            .fold(1f64, f64::max);
+
+        for (y, value) in buffer[1..height].iter().enumerate() {
+            // normalize the Complex FFT value and use it for the color
+            let color_value = (u8::MAX as f64 * value.re.abs() / max_val) as u8;
+            // update the pixel value
+            // Coordinate {
+            //      x <-- current chunk
+            //      y <-- current row
+            // }
+            *raster.pixel_mut(chunk as i32, y as i32) =
+                SHwb8::new(color_value / 2, color_value / 2, color_value);
         }
-    } else {
-        eprintln!("was not matching bit size");
     }
 
     Ok(image::save_buffer(
